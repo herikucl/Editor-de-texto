b7024e9ebdca4f3b92cc340d2034699a48244000
Herik Sarmento <herik@ucl.br>
2022-06-22 21:21:12 -0300

    Comentários Adicionados

--- Editor de texto/Form1.cs
+++ Editor de texto/Form1.cs
@@ -68,7 +68,7 @@ namespace Editor_de_texto
         public Form1()
         {
             InitializeComponent();
-            for (int i = 0; i < 27; i++)
+            for (int i = 0; i < 27; i++) // Inicializando todas as listas do vetor 
             {
                 Alfabeto[i] = new Lista();
             }
@@ -91,127 +91,127 @@ namespace Editor_de_texto
 
         }
 
-        private void novaToolStripMenuItem_Click(object sender, EventArgs e)
+        private void novaToolStripMenuItem_Click(object sender, EventArgs e) // Apertou o botão novo
         {
-            richTextBox1.Text = " ";
+            richTextBox1.Text = " ";    // Apaga todo o texto 
         }
 
-        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e) // Apertou o botão abrir
         {
-            openFileDialog1.Filter = "Arquivos de texto |*.txt";
+            openFileDialog1.Filter = "Arquivos de texto |*.txt";    // Filtro para buscar apenas arquivos .txt
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                richTextBox1.Text=File.ReadAllText(openFileDialog1.FileName);
+                richTextBox1.Text=File.ReadAllText(openFileDialog1.FileName); // Vai colocar todo o texto do arquivo no editor de texto
             }
         }
 
-        private void salvarToolStripMenuItem_Click(object sender, EventArgs e)
+        private void salvarToolStripMenuItem_Click(object sender, EventArgs e) // Apertou o botão salvar
         {
-            saveFileDialog1.Filter = "Arquivos de texto |*.txt";
+            saveFileDialog1.Filter = "Arquivos de texto |*.txt"; // Filtro para que o arquivo salvo seja do tipo .txt
             saveFileDialog1.FilterIndex = 2;
             if (saveFileDialog1.ShowDialog()==DialogResult.OK)
             {
-                File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text); // Vai escrever todo o conteudo do editor de texto no arquivo
             }
         }
 
-        private void desfazerToolStripMenuItem_Click(object sender, EventArgs e)
+        private void desfazerToolStripMenuItem_Click(object sender, EventArgs e)    // Apertou o botão desfazer
         {
-            richTextBox1.Undo();
+            richTextBox1.Undo();    // Vai desfazer a ultima ação (Ctrl+Z)(
         }
 
-        private void refazerToolStripMenuItem_Click(object sender, EventArgs e)
+        private void refazerToolStripMenuItem_Click(object sender, EventArgs e)  // Apertou o botão refazer
         {
-            richTextBox1.Redo();
+            richTextBox1.Redo();   //  Vai refazer a ultima ação (Ctrl+Y)(
         }
 
-        private void recortarToolStripMenuItem_Click(object sender, EventArgs e)
+        private void recortarToolStripMenuItem_Click(object sender, EventArgs e)    // Apertou o botão recortar
         {
-            richTextBox1.Cut();
+            richTextBox1.Cut();   // Vai recortar o bloco de texto selecionado (Ctrl+X)
         }
 
-        private void copiarToolStripMenuItem_Click(object sender, EventArgs e)
+        private void copiarToolStripMenuItem_Click(object sender, EventArgs e)  // Apertou o botão copiar
         {
-            richTextBox1.Copy();
+            richTextBox1.Copy(); // Vai copiar o bloco de texto selecionado (Ctrl+C)
         }
 
-        private void colarToolStripMenuItem_Click(object sender, EventArgs e)
+        private void colarToolStripMenuItem_Click(object sender, EventArgs e)   // Apertou o botão colar
         {
-            richTextBox1.Paste();
+            richTextBox1.Paste();       // Vai colar o bloco de texto selecionado anteriormente (Ctrl+V)
         }
 
-        private void selecionarTudoToolStripMenuItem_Click(object sender, EventArgs e)
+        private void selecionarTudoToolStripMenuItem_Click(object sender, EventArgs e) // Apertou o botão Selecionar tudo
         {
-            richTextBox1.SelectAll();
+            richTextBox1.SelectAll();   // Vai selecionar todo o conteudo do editor de texto
         }
 
-        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)     //Apertou o botao sobre
         {
             MessageBox.Show("Editor de texto feito em C#, capaz de identificar palavras que não estão presentes em um dicionário e destacando-as.\nFeito por: Herik Lemos e Gabriel Reis","Trabalho AED2 2022/1");
         }
-        private void AtualizarDicionario()
+        private void AtualizarDicionario()  //Função que vai colocar todas as palavras do dicionario em memória
         {
             char[] limitadores = new char[] { ' ', '\r', '\n', ',', '\t' };
-            string txtDicionario = File.ReadAllText(caminho);
-            string[] palavras = txtDicionario.ToLower().Split(limitadores, StringSplitOptions.RemoveEmptyEntries);
+            string txtDicionario = File.ReadAllText(caminho); // Le todo o conteudo do dicionario e salva na variavel 
+            string[] palavras = txtDicionario.ToLower().Split(limitadores, StringSplitOptions.RemoveEmptyEntries);  // Quebra o conteudo do dicionario em palavras
             char a;
             for (int i = 0; i < palavras.Length; i++)
             {
-                a = palavras[i][0];
+                a = palavras[i][0]; // Analisa qual a primeira letra da palavra 
 
-                if ((a >= 97) && (a <= 122)) // A primeira letra está entre a - z
+                if ((a >= 97) && (a <= 122)) // A primeira letra está entre a - z, sendo a=97 e z=122  
                 {
-                    Alfabeto[a-97].inserir(palavras[i]);
+                    Alfabeto[a-97].inserir(palavras[i]); // Insere a palavra no vetor correto de acordo com sua primeira letra 
                 }
                 else
                 {
-                    Alfabeto[26].inserir(palavras[i]);
+                    Alfabeto[26].inserir(palavras[i]);  // Palavras que começam com simbolos ou numeros
                 }
             }
         }
-        private void AddDicionario(string s)
+        private void AddDicionario(string s)    //Função que adiciona novas palavras selecionadas ao dicionário
         {
             StreamWriter Escritor;
 
             Escritor = File.AppendText(caminho);
 
             char[] limitadores = new char[] { ' ', '\r', '\n', ',', '\t' };
-            string[] palavras = s.ToLower().Split(limitadores, StringSplitOptions.RemoveEmptyEntries);
+            string[] palavras = s.ToLower().Split(limitadores, StringSplitOptions.RemoveEmptyEntries); //Quebra o bloco do texto selecionado em palavras que serão adicionados ao dicionario
             for (int i = 0; i < palavras.Length; i++)
             {
                 Escritor.Write(palavras[i] + " ");
             }
             Escritor.Close();
-            AtualizarDicionario();
+            AtualizarDicionario(); // Puxa novamente o dicionário para memória
             MessageBox.Show("Palavra(s) adicionadas com sucesso!","Dicionario modificado");
         }
-        private void Marcar(string a)
+        private void Marcar(string a)   // Função que marcará as palavras que não estão presentes no dicionário 
         {
             while (inicio < richTextBox1.TextLength)
             {
-                int inicioPalavra = richTextBox1.Find(a, inicio, RichTextBoxFinds.WholeWord);
-                if (inicioPalavra != -1)
+                int inicioPalavra = richTextBox1.Find(a, inicio, RichTextBoxFinds.WholeWord); // Decobrirá em qual posição do editor está o começo da palavra buscada
+                if (inicioPalavra != -1)    // Palavra foi encontrada
                 {
-                    richTextBox1.SelectionStart = inicioPalavra;
-                    richTextBox1.SelectionLength = a.Length;
-                    richTextBox1.SelectionBackColor = Color.Coral;
-                    inicio = inicioPalavra + a.Length;
-                    break;
+                    richTextBox1.SelectionStart = inicioPalavra;    // Seleciona o incio da palavra
+                    richTextBox1.SelectionLength = a.Length;        // Até o final dela   
+                    richTextBox1.SelectionBackColor = Color.Coral;  // E marca ela com a cor coral
+                    inicio = inicioPalavra + a.Length;              // Para a proxima busca começar a partir do final da anterior
+                    break;                                          // Depois de encontrar a palavra foge do while para marcar apenas uma palavra de cada vez    
                 }
-                else
+                else    // Palavra não foi encontrada
                 {
-                    break;
+                    break;                                  
                 } 
             }
-            richTextBox1.DeselectAll();
+            richTextBox1.DeselectAll(); // Ao final da analise deseleciona qualquer texto que está selecionado   
         }
 
-        private void verificarToolStripMenuItem_Click(object sender, EventArgs e)
+        private void verificarToolStripMenuItem_Click(object sender, EventArgs e)   // Apertou o botão Verificar
         {
             inicio = 0;
-            richTextBox1.SelectAll();
-            richTextBox1.SelectionBackColor = Color.White;
-            string txtEditor = richTextBox1.Text;
+            richTextBox1.SelectAll();                       // Seleciona todo o texto 
+            richTextBox1.SelectionBackColor = Color.White;  // E pinta de branco para fazer a verificação novamente
+            string txtEditor = richTextBox1.Text;           // Puxa o texto do editor de texto para a variavel 
             char[] limitadores = new char[] { ' ', '\r', '\n', ',', '\t' };
             string[] palavras = txtEditor.ToLower().Split(limitadores,StringSplitOptions.RemoveEmptyEntries);
             char a;
@@ -236,19 +236,19 @@ namespace Editor_de_texto
             }
         }
 
-        private void fonteToolStripMenuItem1_Click(object sender, EventArgs e)
+        private void fonteToolStripMenuItem1_Click(object sender, EventArgs e)    //Apertou o botao fonte
         {
             fontDialog1.ShowDialog();
             richTextBox1.SelectionFont = fontDialog1.Font;
         }
 
-        private void corToolStripMenuItem_Click(object sender, EventArgs e)
+        private void corToolStripMenuItem_Click(object sender, EventArgs e)      //Apertou o botao cor
         {
             colorDialog1.ShowDialog();
             richTextBox1.SelectionColor = colorDialog1.Color;
         }
 
-        private void adicionarAoDicionárioToolStripMenuItem_Click(object sender, EventArgs e)
+        private void adicionarAoDicionárioToolStripMenuItem_Click(object sender, EventArgs e)  //Selecionou um bloco de texto e pressionou o botão direito
         {
             AddDicionario(richTextBox1.SelectedText);
         }
